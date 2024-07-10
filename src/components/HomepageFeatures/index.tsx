ae05475fe6513af8263a09be42521e2db493702e
greyhao <buzhiming_hao@163.com>
2024-07-10 10:54:10 +0800

    create project;

---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,68 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
+
+type FeatureItem = {
+  title: string;
+  // Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  description: JSX.Element;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: '👀',
+    description: (
+      <>
+        需要帮助？ 👉
+      </>
+    ),
+  },
+  {
+    title: '联系我',
+    description: (
+      <>
+        <Link href='https://x.com/redear_dev'>Twitter</Link><br />
+      </>
+    ),
+  },
+
+  // {
+  //   title: 'Powered by React',
+  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+  //   description: (
+  //     <>
+  //       Extend or customize your website layout by reusing React. Docusaurus can
+  //       be extended while reusing the same header and footer.
+  //     </>
+  //   ),
+  // },
+];
+
+function Feature({ title, description }: FeatureItem) {
+  return (
+    <div className={clsx('col col--6')}>
+      <div className="text--center">
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/pages/about.tsx
@@ -0,0 +1,10 @@
+import Layout from "@theme/Layout";
+
+
+export default function About(): JSX.Element {
+  return (
+    <Layout>
+      
+    </Layout>
+  );
+}
